ba602369a511b8928521257341a2888d26fdddca
ADITI YADAV <72371906+aditi1403@users.noreply.github.com>
2020-11-14 10:12:03 +0530

    Create box_printing.cpp

---
+++ box_printing.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int row,col;
+    cout<<"Enter the number of rows you want:"<<endl;
+    cin>>row;
+    cout<<"Enter the number of columns you want:"<<endl;
+    cin>>col;
+    for ( int i = 1; i <=row; i++){
+        for ( int j = 1; j <=col; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
