a9bc2bf1d382388770a5fec921fb9ea0812367fa
ADITI YADAV <72371906+aditi1403@users.noreply.github.com>
2020-11-14 10:16:16 +0530

    Create num_L_triangle_print.cpp

---
+++ num_L_triangle_print.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number of rows you want to print:"<<endl;
+    cin>>n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {cout<<i<<" ";
+        
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
