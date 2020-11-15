ef1493ca184c3b51859db810d053dd958493efb7
ADITI YADAV <72371906+aditi1403@users.noreply.github.com>
2020-11-15 17:15:57 +0530

    Update hollow_box_printing.cpp

--- hollow_box_printing.cpp
+++ hollow_box_printing.cpp
@@ -11,17 +11,22 @@ int main(){
 
     for (int i = 1; i <= row; i++)
     {
-        for (int j = 1; i <= col; i++)
+        for (int j = 1; j <= col; j++)
         {
-            if (i==1 || i==row || j==1 || j== col)            {
+            if (i==1 || i==row )
+            {
                 cout<<"* ";
             }
-            else{
-                cout<<" ";
+            else if (j==1 || j==col)
+            {
+                cout<<"* ";
+            }
+            else
+            {
+                cout<<"  ";
             }
         }
         cout<<endl;
     }
-    
     return 0;
 }
