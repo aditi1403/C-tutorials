0411f101e73be249ec89a177e62fdb192c1dffb3
ADITI YADAV <72371906+aditi1403@users.noreply.github.com>
2020-11-16 13:28:31 +0530

    Create armstrong_num_check.cpp

---
+++ armstrong_num_check.cpp
@@ -0,0 +1,35 @@
+// CHECK IF A GIVEN NUMBER IS ARMSTRONG NUMBER OR NOT
+// armstrong number is number whose when eacd digit is 
+// cubed and added gives the same number
+// ex. 153(1+125+27=153)
+
+// ERROR IN THE PROGRAM !!!!
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number you want to check:"<<endl;
+    cin>>n;
+
+    int sum=0;
+    int originaln=n;
+    while (n>0)
+    {
+        int lastdigit=n%10;
+        sum += pow(lastdigit,3);
+        n=n/10;
+    }
+    if (sum==originaln)
+    {
+        cout<<"This is an Armstrong number!!"<<endl;
+    }
+    else
+    {
+        cout<<"Not an armstrong number :(";
+    }
+    
+    return 0;
+}
