2f368d94cad477f174043b10caac0ffb15679a4a
ADITI YADAV <72371906+aditi1403@users.noreply.github.com>
2020-11-16 12:21:12 +0530

    Create reverse_num_print.cpp

---
+++ reverse_num_print.cpp
@@ -0,0 +1,19 @@
+//REVERSE A GIVEN NUMBER 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter th enumber you want to reverse:"<<endl;
+    cin>>n;
+
+    int reverse=0;
+    while (n>0)
+    {
+        int lastdigit=n%10;
+        reverse = reverse*10 + lastdigit;
+        n=n/10;
+    }
+    cout<<reverse<<endl;
+    return 0;
+}
