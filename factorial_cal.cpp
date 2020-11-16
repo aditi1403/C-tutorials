e4ce0cee97685388d87aba082186128a810cee86
ADITI YADAV <72371906+aditi1403@users.noreply.github.com>
2020-11-16 18:15:45 +0530

    Create factorial_cal.cpp

---
+++ factorial_cal.cpp
@@ -0,0 +1,29 @@
+//factorial of two number
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, m;
+    cout<<"Enter the you first number:"<<endl;
+    cin>>n;
+    cout<<"Enter the second number:"<<endl;
+    cin>>m;
+
+    // int x,y;
+    int x=1;
+    for (int i = 1; i <=n ; i++)
+    {
+        x=x*i;
+        
+    }
+    int y=1;
+    for (int j = 1; j <= m; j++)
+    {
+        y=y*j;
+        
+    }
+    cout<<"your first factorial value is:"<<x<<endl;
+    cout<<"your second factorial value is:"<<y<<endl;
+    return 0;
+}
