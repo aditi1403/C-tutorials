d6b3494c1e6ec4eb376d7716ffa9967d8fac1cfa
ADITI YADAV <72371906+aditi1403@users.noreply.github.com>
2020-11-18 17:07:44 +0530

    Create space_complexity.cpp

---
+++ space_complexity.cpp
@@ -0,0 +1,18 @@
+// Space Complexity
+
+// Space complexity of an algorithm quantifies the
+// amount of time taken by a program to run as a function
+// of length of the input. It is directly proportional to
+// the largest memory your program aquires at any
+// instance during run time. 
+
+//Example:
+int n;//4 byte
+cin>>n;
+int a=0;//4 byte
+for (int i = 1; i <=n; i++)//4 byte
+{
+    a=a+1;
+}
+// Hence this program will always aqire a total
+// of 12 bytes in total of the memory
