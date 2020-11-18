c14ffb91e74aadad7743f0213aad0cdcf17ef27b
ADITI YADAV <72371906+aditi1403@users.noreply.github.com>
2020-11-18 17:04:42 +0530

    Create decimal_theory.cpp

---
+++ decimal_theory.cpp
@@ -0,0 +1,17 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     // hexadecimal number is a number with base 6
+//     // octadecimal number is a number with base 8
+//     // hexadecimal number is build up of the following:
+//     // {0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F}
+//     // [A=10;B=11,C=12,D=13,E=14,F=15]
+//     // octadecimal number is build up of the following:
+//     // {0,1,2,3,4,5,6,7}
+//     // octadecimal representation:
+//     // (x)8.....(8 IS IN SUBSCRIPT)
+//     //hexadecimal representation:
+//     // (X)16....(16 IS IN SUBSCRIPT)
+//     return 0;
+// }
