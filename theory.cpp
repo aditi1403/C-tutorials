ff60487888c358c41a1dee2a91e7ab2fc4f6eef3
ADITI YADAV <72371906+aditi1403@users.noreply.github.com>
2020-11-18 17:03:40 +0530

    Create theory.cpp

---
+++ theory.cpp
@@ -0,0 +1,33 @@
+// Space Complexity
+
+// Space complexity of an algorithm quantifies the
+// amount of time taken by a program to run as a function
+// of length of the input. It is directly proportional to
+// the largest memory your program aquires at any
+// instance during run time. 
+
+//Example:
+int n;//4 byte
+cin>>n;
+int a=0;//4 byte
+for (int i = 1; i <=n; i++)//4 byte
+{
+    a=a+1;
+}
+// Hence this program will always aqire a total
+// of 12 bytes in total of the memory
+
+//Case Complexity
+
+// # Worst Case[O(big Oh) notation]
+// # Best Case[_(big omega) notation]
+// # Average Case[_(big theta) notation]
+
+// Time Complexity Calculation
+// .....To be continued
+
+//Comparision of Functions
+// * n...this is a variable and we can assign numric values to it as in maths.
+// * logn...this is similar to the logarithmic function in maths.
+// * sqrt(n)...this is similar to the square root function in maths.
+// * logn...this is similar to logarithmic function in maths.
