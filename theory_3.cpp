6fde84b6c786cc0265ed6013b8ef222afbb65d3e
ADITI YADAV <72371906+aditi1403@users.noreply.github.com>
2020-11-20 21:13:14 +0530

    Create theory_3.cpp

---
+++ theory_3.cpp
@@ -0,0 +1,6 @@
+// SELECTION SORT
+// Find the minimum element in unsorted array and
+// swap it with element at begining
+
+// BUBBLE SORT
+//Reapetedly swap two adjacent elements if they are in wrong order
