799ff972982714809f25713a88c3ad2762890a4a
ADITI YADAV <72371906+aditi1403@users.noreply.github.com>
2020-11-20 10:32:58 +0530

    Create sub_theory.cpp

---
+++ sub_theory.cpp
@@ -0,0 +1,12 @@
+// SUBARRAY
+// continuous part of the array
+// number of sunarrays of an array with n elements= nC2+n=n*(n+1)/2.
+
+// SUBSEQUENCE
+// a sub sequence is a sequence that can be derived an array by
+// selecting zero or more elements, without changing the order
+// of remaining elements
+// number of subsequences of an array with n elemnets= 2^n.
+
+// QUOTE
+// * "Every subarray is a subsequence but every subsequence is not a subarray" *
