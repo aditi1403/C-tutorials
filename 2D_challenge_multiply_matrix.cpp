00e29dc1570abe5f0b3ff1a841f8c17a2b460125
ADITI YADAV <72371906+aditi1403@users.noreply.github.com>
2020-11-27 12:21:54 +0530

    Create 2D_challenge_multiply_matrix.cpp

---
+++ 2D_challenge_multiply_matrix.cpp
@@ -0,0 +1,68 @@
+// 2-D_CHALLENGES(#2)--> MATRIX MULTIPLICATION
+/* will help in topics like-->MATIRX EXPONENTIATION
+    Problem:
+        Given two 2-Dimensional arrays of size n1Xn2 and n2Xn3. Your
+        task is to multiply these matrices and output the multiplied 
+        matrux.
+
+    Constraints:
+        1<=n1,n2,n3<=300
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n1,n2,n3;
+    cout<<"Enter the value of n1, n2 and n3 respectively:"<<endl;
+    cin>>n1>>n2>>n3;
+
+    int m1[n1][n2];
+    int m2[n2][n3];
+    cout<<"Enter matrix 1:"<<endl;
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < n2; j++)
+        {
+            cin>>m1[i][j];
+        }
+    }
+    cout<<"Enter matrix 2:"<<endl;
+    for (int i = 0; i < n2; i++)
+    {
+        for (int j = 0; j < n3; j++)
+        {
+            cin>>m2[i][j];
+        }
+    }
+    int ans[n1][n3];
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < n3; j++)
+        {
+            ans[i][j]=0;
+        }
+    }
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < n3; j++)
+        {
+            for (int k = 0; k < n2; k++)
+            {
+                ans[i][j] += m1[i][k]*m2[k][j];
+            }
+        }
+    }
+    cout<<"Your multiplication ans:"<<endl;
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < n3; j++)
+        {
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
