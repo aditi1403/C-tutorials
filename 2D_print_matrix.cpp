acfe45b41c21a0ebd0e77d724cdb63e70d030971
ADITI YADAV <72371906+aditi1403@users.noreply.github.com>
2020-11-27 11:15:25 +0530

    Create 2D_print_matrix.cpp

---
+++ 2D_print_matrix.cpp
@@ -0,0 +1,37 @@
+// 2-DIMENSIONAL ARRAY
+/* Will be used in:
+     1.Graphs
+     2.Dynamic programming */
+
+ // PRINTING A MATRIX
+
+ #include<iostream>
+ using namespace std;
+
+ int main(){
+     int n,m;
+     cout<<"Enter the value of n:"<<endl;
+     cin>>n;
+     cout<<"Enter the value of m:"<<endl;
+     cin>>m;
+     int arr[n][m];
+     cout<<"Enter the elements of each row of the matrix:"<<endl;
+     for (int i = 0; i < n; i++)
+     {
+         for (int j = 0; j < m; j++)
+         {
+             cin>>arr[i][j];
+         }
+     }
+     cout<<"Matrix is:"<<endl;
+     for (int i = 0; i < n; i++)
+     {
+         for (int j = 0; j < m; j++)
+         {
+             cout<<arr[i][j]<<" ";
+         }
+         cout<<endl;
+     }
+     cout<<endl;
+     return 0;
+ }
