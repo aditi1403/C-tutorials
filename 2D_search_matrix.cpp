53ccb8ffa13913c95a5c0828cf966260e662f7cf
ADITI YADAV <72371906+aditi1403@users.noreply.github.com>
2020-11-27 11:16:34 +0530

    Create 2D_search_matrix.cpp

---
+++ 2D_search_matrix.cpp
@@ -0,0 +1,64 @@
+// 2-DIMENSIONAL ARRAY
+/* Will be used in:
+    1.Graphs
+    2.Dynamic programming */
+
+// SEARCHING IN MATRIX
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cout<<"Enter the value of n:"<<endl;
+    cin>>n;
+    cout<<"Enter the value of m:"<<endl;
+    cin>>m;
+    int arr[n][m];
+    cout<<"Enter the elements of the mtrix:"<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin>>arr[i][j];
+        }
+        
+    }
+    cout<<"Your matrix is as follows:"<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    int x;
+    cout<<"Enter the value of x:"<<endl;
+    cin>>x;
+    cout<<"Location of element:"<<endl;
+    bool flag=false;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (arr[i][j]==x)
+            {
+                cout<<i<<" "<<j<<endl;
+                flag=true;
+            }
+        }
+    }
+    if (flag)
+    {
+        cout<<"Element is found! :) "<<endl;
+    }
+    else
+    {
+        cout<<"Element is not found! :("<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
