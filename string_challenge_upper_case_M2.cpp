24753ed5f3facf0ab0669c8984e377f18a72c4e1
ADITI YADAV <72371906+aditi1403@users.noreply.github.com>
2020-11-29 18:10:41 +0530

    Create string_challenge_upper_case_M2.cpp

---
+++ string_challenge_upper_case_M2.cpp
@@ -0,0 +1,29 @@
+// STRINGS V/S CHARACHTER ARRAYS
+/* CHARACHTER ARRAYS:+ Need to know size beforehand.
+                     + Larger size required for operations
+                       (concatenate or append).
+                     + No terminating extra character.
+   STRINGS:+ No need to know size beforehand.
+           + Performing operations ike concatenation 
+             & append is easier.
+           + Terminated with a special character '\0'.
+*/
+
+// CONVERT TO UPPER CASE-->M2
+
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    string s;
+    cout<<"Enter your word(lower case):"<<endl;
+    getline(cin,s);
+
+    transform(s.begin(),s.end(),s.begin(),::toupper);
+    cout<<"Your word in upper case:"<<endl;
+    cout<<s<<endl;
+    
+    return 0;
+}
