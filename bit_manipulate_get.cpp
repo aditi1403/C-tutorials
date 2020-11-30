e84b88d4e4185877d68fd453bee56a1671b5e913
ADITI YADAV <72371906+aditi1403@users.noreply.github.com>
2020-11-30 15:49:34 +0530

    Create bit_manipulate_get.cpp

---
+++ bit_manipulate_get.cpp
@@ -0,0 +1,17 @@
+// BIT MANIPULATION--> GET BIT
+
+#include<iostream>
+using namespace std;
+
+
+    int getBit(int n, int pos)
+    {
+      return ((n & (1<<pos))!=0);
+    }
+    
+    int main()
+    {
+      cout<<getBit(5,2)<<endl;
+   
+    return 0;
+}
