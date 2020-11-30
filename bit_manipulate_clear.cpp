ef8e376a559494c88f158270e092066a4b7cdf72
ADITI YADAV <72371906+aditi1403@users.noreply.github.com>
2020-11-30 15:43:29 +0530

    Create bit_manipulate_clear.cpp

---
+++ bit_manipulate_clear.cpp
@@ -0,0 +1,30 @@
+// BIT MANIPULATION-CLEAR BIT
+
+#include<iostream>
+using namespace std;
+
+
+    int getBit(int n, int pos)
+    {
+      return ((n & (1<<pos))!=0);
+    }
+
+    int setBit(int n, int pos)
+    {
+      return (n | (1<<pos));
+    }
+
+    int clearBit( int n, int pos)
+    {
+      int mask= ~(1<<pos);
+      return (n& mask);
+    }
+
+    int main()
+    {
+      // cout<<getBit(5,2)<<endl;
+      // cout<<setBit(5,1)<<endl;
+      cout<<clearBit(5,2)<<endl;
+    
+      return 0;
+}
