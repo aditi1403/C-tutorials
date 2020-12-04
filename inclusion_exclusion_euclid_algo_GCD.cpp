f08c554191413f3059344211125f5523d1b89a43
ADITI YADAV <72371906+aditi1403@users.noreply.github.com>
2020-12-04 15:01:40 +0530

    Create inclusion_exclusion_euclid_algo_GCD.cpp

---
+++ inclusion_exclusion_euclid_algo_GCD.cpp
@@ -0,0 +1,26 @@
+//EUCLID ALGORITH TO FIND GCD
+#include<iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    while(b!=0)
+    {
+        int rem=a%b;
+        a=b;
+        b=rem;
+    }
+    return a;
+}
+
+int main()
+{
+    int a,b;
+    cout<<"Enter the value of a:"<<endl;
+    cin>>a;
+    cout<<"Enter the value of b:"<<endl;
+    cin>>b;
+    cout<<"Your ans is as follows:"<<endl;
+    cout<<gcd(a,b)<<endl;
+    return 0;
+}
