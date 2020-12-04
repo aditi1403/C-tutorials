e685dbcd0f6ce2e0995473f0bebe4ed16f1edf00
ADITI YADAV <72371906+aditi1403@users.noreply.github.com>
2020-12-04 14:34:19 +0530

    Create sieve_eratosthenes_prime_factor.cpp

---
+++ sieve_eratosthenes_prime_factor.cpp
@@ -0,0 +1,41 @@
+// SIEVE OR ERATOSTHENES-PRIME FACTOR
+
+#include<iostream>
+using namespace std;
+
+void primefactor(int n)
+{
+    int spf[100]={0};
+    for (int i = 2; i <= n; i++)
+    {
+        spf[i]=i;
+    }
+    for (int i = 2; i <= n; i++)
+    {
+        if (spf[i]==i)
+        {
+            for (int j = i*i; j <= n; j+=i)
+            {
+                if (spf[j]==j)
+                {
+                    spf[j]=i;
+                }
+            }
+        }
+    }
+    while (n!=1)
+    {
+        cout<<spf[n]<<" ";
+        n=n/spf[n];
+    }
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the value of n:"<<endl;
+    cin>>n;
+
+    primefactor(n);
+return 0;
+}
