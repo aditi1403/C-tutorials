6ce0bbe2bb3abe8d18d36cd6226e920abd3a2ede
ADITI YADAV <72371906+aditi1403@users.noreply.github.com>
2020-12-05 15:28:56 +0530

    Create recursion_power_n.cpp

---
+++ recursion_power_n.cpp
@@ -0,0 +1,27 @@
+// RECURSION-RAISED TO THE POWER N
+#include<iostream>
+using namespace std;
+
+int power(int n, int p)
+{
+    if (p==0)
+    {
+        return 1;
+    }
+    int prevPower= power(n,p-1);
+    {
+    return n*prevPower;
+    }
+}
+
+int main()
+{
+    int n,p;
+    cout<<"Enter the value of n:"<<endl;
+    cin>>n;
+    cout<<"Enter the value of p:"<<endl;
+    cin>>p;
+    cout<<"The value of n^p is:"<<endl;
+    cout<<power(n,p)<<endl;
+    return 0;
+}
